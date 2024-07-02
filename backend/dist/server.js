118812169217b483538c41d84b11e746b6e228c8
sushantcp <sushant.kumar@commercepundit.com>
2024-07-02 17:50:33 +0530

    removed files

--- backend/dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = require("./app");
-const PORT = process.env.PORT || 5000;
-app_1.server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
